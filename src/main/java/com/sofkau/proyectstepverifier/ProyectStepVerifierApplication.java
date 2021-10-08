1bacdd4f7a1e8c6adce5166ed3aa9e8e42c8a460
Cgalvispadilla <fktcg99@gmail.com>
2021-10-08 12:45:55 -0500

    se creo el servicio y los test de este con stepVerifier

---
+++ src/main/java/com/sofkau/proyectstepverifier/ProyectStepVerifierApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.proyectstepverifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectStepVerifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectStepVerifierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sofkau/proyectstepverifier/ProyectStepVerifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.proyectstepverifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectStepVerifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
